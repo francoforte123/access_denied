0c050354e87a56b9617a620d5493b1049e902d7d
Emmanuele La Duca <laducaemanuele20@gmail.com>
2023-06-21 16:15:30 +0200

    Aggiungere i file di progetto.

---
+++ access_denied/ClassAccess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace access_denied
+{
+    internal class ClassAccess{
+
+        public int number1= 1;
+        private int number2 = 2;
+        protected int number3 = 3;
+        internal int number4 = 4;
+
+        public int getNumber2 { get => number2; }
+        public int getNumber3 { get => number3; }
+    }
+}
---
+++ access_denied/Program.cs
@@ -0,0 +1,31 @@
+﻿using access_denied;
+
+ClassAccess classAccess = new ClassAccess();
+
+Console.WriteLine("sto provando ad accedere alle proprietà della classe:");
+
+Console.WriteLine(classAccess.number1);
+Console.WriteLine(classAccess.getNumber2);
+Console.WriteLine(classAccess.getNumber3);
+Console.WriteLine(classAccess.number4);
+
+
+//usando i modificatori d'accesso (private, protected), non sono riuscito ad accedere alla variabile, dunque si deve usare 
+//l'incapsulamento usando il "get" (getter) per poter accedere alla variabile 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
